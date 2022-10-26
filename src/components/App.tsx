befaaaab7776192bee2fd662202966d6486539aa
MehmetZiya <mehmetziya82@gmail.com>
2022-10-26 11:14:46 +0200

    added style

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,11 +2,12 @@ import React from 'react'
 import { Provider } from 'react-redux'
 import { store } from '../state'
 import RepositoriesList from './RepositoriesList'
+import '../index.css'
 
 const App = () => {
   return (
     <Provider store={store}>
-      <div>
+      <div id='App'>
         <h1>Search For a Package</h1>
         <RepositoriesList />
       </div>
